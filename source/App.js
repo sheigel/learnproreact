18f3f8ca4da161c062e7ce6270e06746478ffa97
Andrei Gherghel <andrei.gherghel@hotmail.com>
2016-01-10 23:56:19 +0200

    3. Intial Kanban Board

--- source/App.js
+++ source/App.js
@@ -1,29 +1,38 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import KanbanBoard from './KanbanBoard';
 
-// Grocery App
-// Parent Component
-class GroceryList extends Component {
-  render() {
-    return (
-      <ul>
-        <ListItem quantity="1">Bread</ListItem>
-        <ListItem quantity="6">Eggs</ListItem>
-        <ListItem quantity="2">Milk</ListItem>
-      </ul>
-    );
-  }
-}
+let cardsList = [
+  {
+    id: 1,
+    title: "Read the Book",
+    description: "I should read the whole book",
+    status: "in-progress",
+    tasks: []
+  },
+  {
+    id: 2,
+    title: "Write some code",
+    description: "Code along with the samples in the book",
+    status: "todo",
+    tasks: [
+      {
+        id: 1,
+        name: "ContactList Example",
+        done: true
+      },
+      {
+        id: 2,
+        name: "Kanban Example",
+        done: false
+      },
+      {
+        id: 3,
+        name: "My own experiments",
+        done: false
+      }
+    ]
+  },
+];
 
-//Child Component
-class ListItem extends Component {
-  render() {
-    return (
-      <li>
-        {this.props.quantity} × {this.props.children}
-      </li>
-    );
-  }
-}
-
-ReactDOM.render(<GroceryList />, document.getElementById('root'));
+ReactDOM.render(<KanbanBoard cards={cardsList} />, document.getElementById('root'));
